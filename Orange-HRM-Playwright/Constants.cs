a671a2efc027031e6066f100693af6b37597bccd
Hazim Okanovic <hazimokanovic@live.com>
2023-01-31 16:43:53 +0100

    improved login tests with constant variables

--- Orange-HRM-Playwright/Constants.cs
+++ Orange-HRM-Playwright/Constants.cs
@@ -2,6 +2,13 @@ namespace Orange_HRM_Playwright
 {
     public class Constants
     {
-        
+        public const string Required = "Required";
+        public const string ValidPassword = "admin123";
+        public const string ValidUsername = "Admin";
+        public const string IncorrectUsername = "Hazim";
+        public const string IncorrectPassword = "adminadmin";
+        public const string CredentialsError = "Invalid credentials";
+        public const string DashboardTitle = "Dashboard";
+        public const string LoginTitle = "Login";
     }
 }
